8a768e54b6232b3129313534aee294bced768ff9
lilu-ps <48617956+lilu-ps@users.noreply.github.com>
2023-07-23 09:50:14 +0400

    employee info page mockup

---
+++ GalaxyTaxi.Shared/Api/Models/EmployeeManagement/EmployeeJourneyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GalaxyTaxi.Shared.Api.Models.EmployeeManagement
+{
+    public class EmployeeJourneyInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Mobile { get; set; }
+    }
+}

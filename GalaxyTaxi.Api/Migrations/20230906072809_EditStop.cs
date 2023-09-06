7a4d0a1bfffd386e9b76118aeb19f95ffdbff247
anoddos <48618560+anoddos@users.noreply.github.com>
2023-09-06 15:28:41 +0800

    Edit Stop

---
+++ GalaxyTaxi.Api/Migrations/20230906072809_EditStop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GalaxyTaxi.Api.Migrations
+{
+    public partial class EditStop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "PickupTime",
+                table: "Stops",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PickupTime",
+                table: "Stops",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+        }
+    }
+}
